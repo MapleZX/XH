552af2b9a8d3df7a354afc9b4f7ec87dcbe44fc9
MarisaBroom <fengye5412@outlook.com>
2021-02-04 09:21:53 +0800

    Delete XH directory

--- XH/XH/Context/Interfaces/IContextObject.cs
+++
@@ -1,9 +0,0 @@
-namespace XH
-{
-    public interface IContextObject
-    {
-        IService GetService<IService>() where IService : class;
-        IService GetService<IService>(string object_name) where IService : class;
-        void ScopedEnd();
-    }
-}
--- XH/XH/Context/Interfaces/ICreator.cs
+++
@@ -1,8 +0,0 @@
-using XH.Container;
-namespace XH
-{
-    public interface ICreator
-    {
-        void Register(IServiceCollection service);
-    }
-}
